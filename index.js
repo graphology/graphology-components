232ed914edf73487589dee2505a912212c52e57d
Tanguy LUCCI <lcc.tanguy@gmail.com>
2017-01-11 15:04:29 +0100

    Fixed #2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function connectedComponents(graph) {
     return [];
 
   if (!graph.size)
-    return graph.nodes();
+    return graph.nodes().map(function(node) { return [node]; });
 
   var components = [],
       component,
